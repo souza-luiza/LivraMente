a15bf5e2bae00418e5f09b65cd98e7666003be79
isabele <isabelelima614@gmail.com>
2025-09-30 06:32:09 -0300

    adicionada função handleSubmit, que envia os dados de login para API (se válido) e trata respostas

--- livrafront/src/forms/useLoginForm.tsx
+++ livrafront/src/forms/useLoginForm.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import { loginSchema } from '@/lib/validations/auth'
+import { loginUser } from '@/services/auth'
 
 export function useLoginForm() {
   // Dados do formulário e mensagens de erro
   const [formData, setFormData] = useState({ email: '', password: '' })
   const [errors, setErrors] = useState<{email?: string, password?: string}>({})
+  // Estado de carregamento e erro da API
+  const [isLoading, setIsLoading] = useState(false)
+  const [apiError, setApiError] = useState<string>('')
 
-  // Atualiza campo e valida com a biblioteca Zod
+  // Atualiza campos e valida com a biblioteca Zod
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
     setFormData(prev => ({ ...prev, [name]: value }))
@@ -21,7 +25,35 @@ export function useLoginForm() {
     }
   }
 
+  // Envia dados para API e trata respostas
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    setIsLoading(true)
+    setApiError('')
+
+    try {
+      loginSchema.parse(formData)
+      const response = await loginUser(formData)
+
+      alert(`Bem-vindo ${response.user.username}`)
+      console.log('Token recebido:', response.token)
+      
+    } catch (error: any) {
+      if (error.errors) {
+        const fieldErrors: any = {}
+        error.errors.forEach((err: any) => {
+          fieldErrors[err.path[0]] = err.message
+        })
+        setErrors(fieldErrors)
+      } else {
+        setApiError(error.message)
+      }
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return { 
-    formData, errors, handleChange 
+    formData, errors, handleChange, handleSubmit, isLoading, apiError
   }
 }
