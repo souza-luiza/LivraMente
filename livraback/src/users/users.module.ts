6b915faea07f41bb1580946947c69b57da3edda0
Luiza de Souza <souza.luiza@unifesp.br>
2025-10-01 11:53:42 -0300

    Adição de funções CRUD.
    Conserto de id como number para string, importação do schema de User e adição de funções de criar usuário, buscar todos usuários, buscar um usuário, atualizar usuário e remover usuário.

--- livraback/src/users/users.module.ts
+++ livraback/src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './entities/user.entity';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])], // name acessa o nome da classe como string
   controllers: [UsersController],
   providers: [UsersService],
 })
--- livraback/src/users/users.service.ts
+++ livraback/src/users/users.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
+
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {} // dentro dessa variavel tem todos os metodos do mongo
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user = new this.userModel(createUserDto); // insere variaveis no modelo
+    return user.save(); // salva usuario
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userModel.find(); // retorna lista
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) { // id do mongo eh uma string
+    return this.userModel.findById(id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate(
+      {
+        _id: id, // procurar objeto por id (mongo por padrao cria id com _)
+      }, 
+      {
+        $set: updateUserDto, // o que quero alterar (set altera os campos que eu quero alterar)
+      },
+      {
+        new: true, // alterar no banco de dados
+      },
+    );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.userModel.deleteOne({
+      _id: id,
+    }).exec(); // para executar operacao
   }
 }
