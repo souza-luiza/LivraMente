5452006d4c1c6307a449f9a9b85e94dee6d3eff0
Luiza de Souza <souza.luiza@unifesp.br>
2025-10-01 11:48:13 -0300

    Schema provisório de User

--- livraback/src/users/entities/user.entity.ts
+++ livraback/src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {} //collections dentro do mongo
+//collections dentro do mongo
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>; // referencia de userDocument fica User
+
+@Schema()
+export class User {
+    @Prop()
+    name: string;
+
+    @Prop() // "colunas" da tabela
+    email: string;
+
+    @Prop()
+    password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
