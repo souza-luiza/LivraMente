977be95b2b89fa3970cf0f112f473c1781177f11
Luiza de Souza <souza.luiza@unifesp.br>
2025-10-02 21:03:12 -0300

    Testes para Definição de Controller e Service de users
    Utilizando mock, que é uma simulação de uma dependência real da classe para permitir testes.
    Instalação de pacote para testes.

--- livraback/src/users/users.controller.spec.ts
+++ livraback/src/users/users.controller.spec.ts
@@ -1,15 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ModuleMocker, MockMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-      providers: [UsersService],
-    }).compile();
+      controllers: [UsersController]
+    })
+      .useMocker((token) => {
+        const results = ['test1', 'test2'];
+        if (token === UsersService) {
+          return { findAll: jest.fn().mockResolvedValue(results) };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(
+            mockMetadata,
+          ) as ObjectConstructor;
+          return new Mock();
+        }
+      })
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
--- livraback/src/users/users.service.spec.ts
+++ livraback/src/users/users.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, {provide: getModelToken(User.name), useFactory: jest.fn }], // jest.fn => realiza a criação de uma função mock para schema
     }).compile();
 
     service = module.get<UsersService>(UsersService);
