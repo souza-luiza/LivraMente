d91ffe20c8d8b79c4d7767a23105ac9622adb5dc
João Pedro Zampoli <jpszampoli53@gmail.com>
2025-10-06 21:35:07 -0300

    Config básica da página inicial e da página 404

---
+++ livrafront/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div className="">
+            <h1>Bem-vindo ao LivraMente</h1>
+            <p>A rede dos leitores brasileiros</p>
+        </div>
+    )
+}
