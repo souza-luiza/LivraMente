c293481b951bbb579539c64516d964ad8337013e
isabele <isabelelima614@gmail.com>
2025-10-07 10:31:36 -0300

    habilitar CORS para permitir requisições no front

--- livraback/src/main.ts
+++ livraback/src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // remove propriedades não permitidas
