9ca7c624ad1ee6e9b8c5fcc07d801e67fc1fa608
Luiza de Souza <souza.luiza@unifesp.br>
2025-10-08 23:46:42 -0300

    Arrumando nome da variável do .env

--- livrafront/src/app/api/register/route.ts
+++ livrafront/src/app/api/register/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 
-const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3001'
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001'
 
 export async function POST(req: Request) {
   try {
