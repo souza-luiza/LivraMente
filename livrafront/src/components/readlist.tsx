91af98ef588cc7e20a44bc94ffc0d1ee0afcca35
João Pedro Zampoli <jpszampoli53@gmail.com>
2025-10-18 11:36:38 -0300

    Ajustando sombra do hover de uma readlist

--- livrafront/src/components/readlist.tsx
+++ livrafront/src/components/readlist.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Readlist({ id, title, author, image }: { id?: string; title?: string; author?: string; image?: string }) {
     return(
-        <Link href={`/readlist/${id}`} className="w-full items-center flex flex-col p-2 pb-4 hover:shadow-lg transition-shadow rounded-lg">
+        <Link href={`/readlist/${id}`} className="w-full items-center flex flex-col p-2 pb-4 hover:shadow-md transition-shadow rounded-lg">
             <div className="relative w-full aspect-[4/4]">
                 <Image 
                     src={image || "/Readlist.svg"} 
