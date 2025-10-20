2fbef713738cad111bb4e1695b98ff48bb2f4cf8
akemiikemoto <leticia.ikemoto04@gmail.com>
2025-10-20 20:27:19 -0300

    Adição de ajustes para o tamanho

--- livrafront/src/components/searchbar.tsx
+++ livrafront/src/components/searchbar.tsx
@@ -2,9 +2,16 @@ import SearchIcon from "./icons/SearchIcon";
 import { InputHTMLAttributes } from 'react';
 
 interface SearchBarProps extends InputHTMLAttributes<HTMLInputElement> {
+  inputSize?: 'small' | 'medium' | 'large'
 }
 
-export default function SearchBar({ className, ...props }: SearchBarProps) {
+export default function SearchBar({ className, inputSize = 'medium', ...props }: SearchBarProps) {
+  const boxSize: Record<'small' | 'medium' | 'large', string> = {
+    small:  "small-box",
+    medium: "medium-box",
+    large:  "large-box"
+  };
+
   return (
     <div className={`relative w-full ${className}`}>
       <span className="absolute left-4 top-1/2 -translate-y-1/2 text-[#95BB7E]">
@@ -13,7 +20,7 @@ export default function SearchBar({ className, ...props }: SearchBarProps) {
 
       <input
         type="text"
-        className="w-full py-3 pl-12 pr-4 bg-[#B0CC9E] text-[#95BB7E] placeholder:text-[#95BB7E] rounded-lg focus:outline-none focus:ring-[#1F2A17]"
+        className={`w-full pl-12 pr-4 bg-[#B0CC9E] text-[#95BB7E] placeholder:text-[#95BB7E] rounded-lg focus:outline-none focus:ring-[#1F2A17] ${boxSize[inputSize]}`}
         {...props}
       />
     </div>
