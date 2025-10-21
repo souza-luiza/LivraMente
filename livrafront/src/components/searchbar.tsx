e9f08cb1a0e244d652a76fdbf69ff0e23abbc050
akemiikemoto <leticia.ikemoto04@gmail.com>
2025-10-21 13:01:26 -0300

    Update searchbar.tsx
    mudança de:
    - cores como dito na sugestão
    - remoção da borda
    - fonte b2

--- livrafront/src/components/searchbar.tsx
+++ livrafront/src/components/searchbar.tsx
@@ -6,13 +6,13 @@ interface SearchBarProps extends InputHTMLAttributes<HTMLInputElement> { }
 export default function SearchBar({ className, ...props }: SearchBarProps) {
   return (
     <div className={`relative w-full max-w-2xl ${className}`}>
-      <span className="absolute left-2 top-1/2 -translate-y-1/2 text-[#3D552F]">
+      <span className="absolute left-2 top-1/2 -translate-y-1/2 text-[#5C8046]">
         <SearchIcon size={20} />
       </span>
 
       <input
         type="text"
-        className="w-full pr-4 bg-[#CADDBF] text-[#1F2A17] placeholder:text-[#3D552F] rounded-lg focus:outline-none focus:ring-[#95BB7E] medium-box text-b3" 
+        className="w-full pr-4 bg-[#CADDBF] text-[#1F2A17] border-none placeholder:text-[#5C8046] focus:outline-none focus:ring-[#95BB7E] medium-box text-b2" 
         style={{ paddingLeft: '2rem' }}
         {...props}
       />
