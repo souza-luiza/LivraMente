ecbcf6ab2407afba7b05ad973b88cd347c0c3fdb
Luiza de Souza <souza.luiza@unifesp.br>
2025-10-24 18:06:27 -0300

    Página de todas as comunidades
    Ainda não finalizada

---
+++ livrafront/src/app/comunidades/page.tsx
@@ -0,0 +1,14 @@
+import Sidebar from "@/components/sidebar";
+import ComunidadesTabs from "./comunidades-tab";
+
+export default function CommunitiesPage() {
+    return (
+        <div className="flex min-h-screen bg-[#E5EEDF]">
+            <Sidebar />
+            <main className="flex-1 flex flex-col p-4">
+                <h2 className="text-h2 mb-4">Comunidades</h2>
+                <ComunidadesTabs />
+            </main>
+        </div>
+    )
+}
