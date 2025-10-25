8bb791ff2452d0474aff18310944051971ee9927
Luiza de Souza <souza.luiza@unifesp.br>
2025-10-25 18:24:02 -0300

    Controller Comunidades
    - Endpoint para retornar posts;
    - Endpoint para retornar membros;
    - Endpoint para adicionar membro;
    - Endpoint para remover membro.

---
+++ livraback/src/comunidades/comunidades.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Delete, Get, Param, Patch, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ComunidadesService } from './comunidades.service';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { CurrentUserDto } from 'src/auth/dto/current-user.dto';
+
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+@Controller('comunidades')
+export class ComunidadesController {
+    constructor(private readonly comunidadesService: ComunidadesService) {}
+
+    @Get(':comunidadeNome/posts')
+    async findAllPosts(@Param('comunidadeNome') comunidadeNome: string) {
+        return this.comunidadesService.findAllPosts(comunidadeNome);
+    }
+
+    @Get(':comunidadeNome/membros')
+    async findAllComunidadeMembros(@Param('comunidadeNome') comunidadeNome: string) {
+        return this.comunidadesService.findAllComunidadeMembros(comunidadeNome);
+    }
+
+    @Patch(':comunidadeNome/membros')
+    async addMembro(@CurrentUser() user: CurrentUserDto, @Param('comunidadeNome') comunidadeNome: string) {
+        return this.comunidadesService.addMembro(user.userId, comunidadeNome);
+    }
+
+    @Delete(':comunidadeNome/membros')
+    async removeMembro(@CurrentUser() user: CurrentUserDto, @Param('comunidadeNome') comunidadeNome: string) {
+        return this.comunidadesService.removeMembro(user.userId, comunidadeNome);
+    }
+}
