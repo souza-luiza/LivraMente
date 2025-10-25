ea43ef9121cde518d46a697898c06e7b1ce3d765
Luiza de Souza <souza.luiza@unifesp.br>
2025-10-25 20:50:32 -0300

    Endpoints de Criar e Atualizar Comunidades

--- livraback/src/comunidades/comunidades.controller.ts
+++ livraback/src/comunidades/comunidades.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Delete, Get, Param, Patch, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { ComunidadesService } from './comunidades.service';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { CurrentUserDto } from 'src/auth/dto/current-user.dto';
+import { CreateComunidadeDto } from './dto/create-comunidade.dto';
+import { UpdateComunidadeDto } from './dto/update-comunidade.dto';
 
 @ApiBearerAuth()
 @UseGuards(JwtAuthGuard)
 @Controller('comunidades')
 export class ComunidadesController {
     constructor(private readonly comunidadesService: ComunidadesService) {}
+
+    @Post()
+    async create(@CurrentUser() user: CurrentUserDto, @Body() createComunidadeDto: CreateComunidadeDto) {
+        return this.comunidadesService.create(user.userId, createComunidadeDto);
+    }
+
+    @Patch(':comunidadeNome')
+    async update(@CurrentUser() user: CurrentUserDto, @Param('comunidadeNome') comunidadeNome: string, @Body() updateComunidadeDto: UpdateComunidadeDto) {
+        return this.comunidadesService.update(user.userId, comunidadeNome, updateComunidadeDto);
+    }
 
     @Get(':comunidadeNome/posts')
     async findAllPosts(@Param('comunidadeNome') comunidadeNome: string) {
