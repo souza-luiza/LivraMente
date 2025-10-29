044dca92bd368e4fd83cabfbacca34fbf0e22b4b
Viviane Park <viviflorp@gmail.com>
2025-10-29 15:10:53 -0300

    renomeação

--- livrafront/test/livratime/livratime.test.tsx
+++ livrafront/test/livratime/livratime.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import LivraTime from '@/app/livratime/page'
+import LivraTime from '@/app/magalu/page'
 
 jest.mock('next/navigation', () => ({
   useRouter: jest.fn(),
