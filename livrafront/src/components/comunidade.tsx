3e1187f7b6169d2c54c5b77c36304f2d19317820
Luiza de Souza <souza.luiza@unifesp.br>
2025-10-31 00:37:58 -0300

    Alteração nas imagens das comunidades

--- livrafront/src/components/comunidade.tsx
+++ livrafront/src/components/comunidade.tsx
@@ -3,23 +3,23 @@ import Link from "next/link";
 
 export default function Comunidade({ id, name, descricao, image}: {id?:string; name?:string; descricao?:string; image?:string }) {
     return (
-        <Link href={`/comunidades/${id}`} className="w-full items-center flex items-center p-4 gap-2 hover:shadow-md transition-shadow rounded-lg">
-                    {/*Coluna da Imagem*/}
-                    <div className="flex-shrink-0">
-                        <Image
-                            src={image || "/Readlist.svg"}
-                            alt={`Imagem da comunidade ${name}`}
-                            width={80}
-                            height={80}
-                            className="object-cover rounded-lg"
-                        />
-                    </div>
+        <Link href={`/comunidades/${id}`} className="w-full flex items-center p-4 gap-2 hover:shadow-md transition-shadow medium-border-radius">
+            {/*Coluna da Imagem*/}
+            <div className="w-1/5 overflow-hidden rounded-full border-[2px]" style={{width: 80, height: 80}}>
+                <Image
+                    src={image || "/team/Kemi.jpg"}
+                    alt={"Imagem da Comunidade"}
+                    width={80}
+                    height={80}
+                    className="object-cover"
+                />
+            </div>
 
-                    {/*Coluna do conteúdo*/}
-                    <div className="flex flex-col">
-                        <h6 className="text-h6 font-semibold">{name || "Nome da comunidade"}</h6>
-                        <p className="text-b3 line-clamp-2">{descricao || "Descrição da comunidade"}</p>  
-                    </div>
+            {/*Coluna do conteúdo*/}
+            <div className="flex flex-col w-4/5 pl-2">
+                <h6 className="text-h6 font-semibold">{name || "Nome da comunidade"}</h6>
+                <p className="text-b3 line-clamp-2">{descricao || "Descrição da comunidade"}</p>  
+            </div>
         </Link>
     )
 }
