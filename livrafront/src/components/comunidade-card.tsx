7f1744469d59b0f6e86831bdae72141747545d80
Luiza de Souza <souza.luiza@unifesp.br>
2025-11-04 18:27:17 -0300

    Ajustando caminho da comunidade

--- livrafront/src/components/comunidade-card.tsx
+++ livrafront/src/components/comunidade-card.tsx
@@ -8,9 +8,18 @@ interface ComunidadeCardProps {
     image?: string;
 }
 
+function titleToSlug(title: string): string {
+  return title
+    .toLowerCase() // transforma tudo em minúsculo
+    .normalize('NFD') // separa os acentos das letras
+    .replace(/[\u0300-\u036f]/g, '') // remove os acentos
+    .replace(/[^a-z0-9]+/g, '-') // substitui tudo que não é letra ou número por hífen
+    .replace(/^-+|-+$/g, ''); // remove hífens do início e do fim
+}
+
 export default function Comunidade({ id, nome, descricao, image }: ComunidadeCardProps) {
     return (
-        <Link href={`/comunidades/${id}`} className="w-full flex items-center p-4 gap-2 hover:shadow-md transition-shadow medium-border-radius">
+        <Link href={`/comunidade/${titleToSlug(nome)}`} className="w-full flex items-center p-4 gap-2 hover:shadow-md transition-shadow medium-border-radius">
             {/*Coluna da Imagem*/}
             <div className="w-1/5 overflow-hidden rounded-full border-[3px] text-[var(--primary-700)] bg-[#3D552F]" style={{width: 80, height: 80}}>
                 {image ? (<Image
