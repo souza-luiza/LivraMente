455b24d2a7061a26eb1a9ffaaeb5edffc8db8101
akemiikemoto <leticia.ikemoto04@gmail.com>
2025-11-16 20:43:57 -0300

    Update llm.controller.ts
    Adição do Endpoint para o agente de análise

--- livraback/src/llm/llm.controller.ts
+++ livraback/src/llm/llm.controller.ts
@@ -1,18 +1,43 @@
-import { Controller, Post, Body, ValidationPipe, UsePipes } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, UsePipes, UseGuards, Req } from '@nestjs/common';
 import { GenerateTextDTO } from './writer/dto/generate-text.dto';
 import { LlmResponseDTO } from './writer/dto/llm-response.dto';
 import { LlmStoryService } from './writer/llm.story.service';
+import { LlmAgentService } from './assistant/llm.agent.service';
+import { AgentInputDto } from './assistant/dto/agent-input.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { CurrentUserDto } from '../auth/dto/current-user.dto';
 
 @Controller('llm')
 export class LlmController {
 
   constructor(
     private readonly storyService: LlmStoryService,
+    private readonly agentService: LlmAgentService,
   ) { }
 
+  // Endpoint para gerar texto (histórias)
   @Post('gerar')
+  @UseGuards(JwtAuthGuard)
   @UsePipes(new ValidationPipe())
-    async gerarTexto(@Body() generateTextDto: GenerateTextDTO): Promise<LlmResponseDTO> {
-      return this.storyService.generateAndSaveStory(generateTextDto);
-    }
+  async gerarTexto(@Body() generateTextDto: GenerateTextDTO): Promise<LlmResponseDTO> {
+    return this.storyService.generateAndSaveStory(generateTextDto);
+  }
+
+  // Endpoint para o agente de análise
+  @Post('analisar')
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(new ValidationPipe())
+  async PromptAnalise(
+    @Body() agentInputDto: AgentInputDto,
+    @CurrentUser() user: CurrentUserDto,
+  ): Promise<{ response: string }> {
+
+    const agentResponse = await this.agentService.runAnalysisAgent(
+      agentInputDto.userPrompt,
+      user.userId,
+    );
+
+    return { response: agentResponse };
+  }
 }
