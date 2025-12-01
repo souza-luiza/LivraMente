731bdb26c339d28d40550ce3405bee5b17c50a1d
isabele <isabelelima614@gmail.com>
2025-12-01 15:04:21 -0300

    atualizando o toast notification para suportar as notificações

--- livrafront/src/components/notificacoes-provider.tsx
+++ livrafront/src/components/notificacoes-provider.tsx
@@ -1,26 +1,14 @@
 'use client';
 
 import { useNotificacoes } from '@/hooks/useNotificacoes';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import ToastNotification from '@/components/toast-notification';
 
 export default function NotificacoesProvider({ children }: { children: React.ReactNode }) {
     useNotificacoes();
     return (
         <>
             {children}
-            <ToastContainer 
-                position="top-right" 
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={true}
-                closeOnClick={true}
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                closeButton={true}
-            />
+            <ToastNotification />
         </>
     );
 }
